0c4dae37a0d848f116ebb98b66197e5763665ae1
Fabio Rodrigez <fabiorodrigez@Fabios-Mac-mini.local>
2019-10-03 15:31:43 -0400

    Hashing
    Added hashing to our log in md5 npm module.

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
+//mongoose encryption module
+//const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
 console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -28,7 +33,8 @@ const userSchema = new mongoose.Schema({
 //defining our secret ? in env file/ scret key
 //const secret = "Thisisourlittlesecret.";
 //process.env.SECRET
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); // encript only certain fields with the mongoose plug in if multiple fields add a coma to the array
+//userSchema below is for plugin is for encrypting fields
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); // encript only certain fields with the mongoose plug in if multiple fields add a coma to the array
 
 
 //calling my userSchema object and const User as the first param
@@ -82,7 +88,7 @@ app.get("/logout", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)//reversible hash//password: req.body.password
     });
 //if our user has been created succesfully in our database, then user will have access to the  secrets.ejs page 
 // when creating new user  .SAVE documents are encrypted and then signed
@@ -99,7 +105,7 @@ newUser.save(function(err){
 //login post route  our log in users in the database
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);  //without md5 req.body.password;
     
 
 //findOne with matching user name and password in the database plus our email field is matching the username field
